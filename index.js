7b6bf3c7b4ae5403f742eb7e8218f51ddfeccbb2
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2022-06-23 15:35:20 -0400

    remove menu mobile js

--- index.js
+++ index.js
@@ -10,7 +10,23 @@ if(navToggle) {
   })
 }
 /*============= MENU HIDDEN ===========*/
+/* Validate if constant exists */
+
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
 /*============= REMOVE MENU MOBILE ===========*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav_link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 /*============= ACCORDIAN SKILLS ===========*/
 /*============= MENU SHOW Y HIDDEN ===========*/
 /*============= MENU SHOW Y HIDDEN ===========*/
