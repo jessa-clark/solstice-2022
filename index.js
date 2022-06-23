d6987b97f7b42980b860aa6df025c6c7bb11f878
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2022-06-23 15:22:51 -0400

    JS show menu toggle, nav styling

---
+++ index.js
@@ -0,0 +1,16 @@
+/*============= MENU SHOW Y HIDDEN ===========*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+/*============= MENU SHOW ===========*/
+/* Validate if constant exists */
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+/*============= MENU HIDDEN ===========*/
+/*============= REMOVE MENU MOBILE ===========*/
+/*============= ACCORDIAN SKILLS ===========*/
+/*============= MENU SHOW Y HIDDEN ===========*/
+/*============= MENU SHOW Y HIDDEN ===========*/
