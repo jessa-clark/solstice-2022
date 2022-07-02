3456c04c65c9e1c74a1da8a3bab7cebb041e5416
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2022-07-02 01:45:21 -0400

    skills js complete, skills section complete

--- index.js
+++ index.js
@@ -28,5 +28,29 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 /*============= ACCORDIAN SKILLS ===========*/
+const skillsContent = document.getElementsByClassName('skills_content'),
+skillsHeader = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+  let itemClass = this.parentNode.className
+
+  for(i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills_content skills_close'
+  }
+  if(itemClass === 'skills_content skills_close'){
+    this.parentNode.className = 'skills_content skills_open'
+  }
+}
+
+skillsHeader.forEach((eL) =>{
+  eL.addEventListener('click', toggleSkills)
+})
+/*============= QUALIFICATION TABS ===========*/
+
+/*============= SERVICES MODAL ===========*/
+
+/*============= PORTFOLIO SWIPER ===========*/
+
 /*============= MENU SHOW Y HIDDEN ===========*/
+
 /*============= MENU SHOW Y HIDDEN ===========*/
